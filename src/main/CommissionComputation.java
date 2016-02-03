e61654519459bc98d71c6f6719bea57892e603b8
jvarone1 <jvarone1@gmail.com>
2016-02-03 18:42:36 -0500

    Updated CommissionComputation with method to get sales and commission for input employee.  Updated jar file

--- src/main/CommissionComputation.java
+++ src/main/CommissionComputation.java
@@ -18,11 +18,10 @@ public class CommissionComputation {
 	private static ArrayList<ArrayList<String>> salesRows = FilesLab.getArrayListFromFile(FilesLabConstants.SALES_INPUT_FILE); //Creates 2D ArrayList with data
 	private static ArrayList<ArrayList<String>> empRows = FilesLab.getArrayListFromFile(FilesLabConstants.EMPLOYEE_INPUT_FILE); //Creates 2D ArrayList with data
 	private static ArrayList<ArrayList<String>> writeArray = new ArrayList<ArrayList<String>>(); //Array to be written to file
+	private static DateFormat format = new SimpleDateFormat("MM/dd/yy"); //converts date strings to dates
 	
 	public static void main(String[] args) throws ParseException, IOException {		
-		ArrayList<Integer> fitCriteria = new ArrayList<Integer>(); //holds values of all included records		
-		DateFormat format = new SimpleDateFormat("MM/dd/yy"); //converts date strings to dates
-		Date readDate; //current dates of records as they are read in
+		//Read in dates from argument values, terminate if invalid
 		Date startDate = new Date(0);
 		Date endDate = new Date(Long.MAX_VALUE);
 		try {
@@ -34,43 +33,16 @@ public class CommissionComputation {
 			System.exit(0);
 		}
 		
-		//Stores locations of included records in fitCriteria
-		for(int i=1; i<salesRows.size(); i++) {
-			readDate = format.parse(salesRows.get(i).get(1));
-			if((readDate.after(startDate) && readDate.before(endDate)) || readDate.equals(startDate) || readDate.equals(endDate)){
-				fitCriteria.add(i);
-			}
-		}		
-		
-		calculateSalesForEmps(fitCriteria);	
+		//Get total sales and commissions for each employee, ignoring duplicate IDs
+		for(int i=1; i<empRows.size(); i++) {
+			int empId = Integer.parseInt(empRows.get(i).get(0));
+			double[] salesCom = getCommForPeriod(empRows.get(i), startDate, endDate);			
+			if(!alreadyInArray(empId)) addToWriteArray(empId, salesCom[0], salesCom[1]);
+		}
+				
 		FilesLab.writeToFile(FilesLabConstants.SALES_OUTPUT_FILE, writeArray);
 		
 		System.out.println("Read " + empRows.size() + " employees");
-	}
-	
-	/**
-	 * Read each employee in employee file and find corresponding sales in sales file.
-	 */
-	public static void calculateSalesForEmps(ArrayList<Integer> fitCriteria) {
-		for(int i=1; i<empRows.size(); i++) {
-			String empPos = empRows.get(i).get(4);
-			int empId = Integer.parseInt(empRows.get(i).get(0));
-			double empSales = 0;
-			double empCom = 0;
-			for(int j=0; j<fitCriteria.size(); j++) {
-				if((Integer.parseInt(salesRows.get(fitCriteria.get(j)).get(0)) == empId ||
-						empPos.equalsIgnoreCase("President") || empPos.equalsIgnoreCase("Sales Manager"))) {
-					empSales += Double.parseDouble(salesRows.get(fitCriteria.get(j)).get(3));
-				}
-			}
-			String tempCom = empRows.get(i).get(7).replace('%', ' ');
-			if(!tempCom.isEmpty()) {
-				empCom = Double.parseDouble(tempCom)/100.0;
-			}
-			empCom = empCom * empSales;
-			if(empSales != 0 && !alreadyInArray(empId))
-				addToWriteArray(empId, empSales, empCom);
-		}
 	}
 	
 	/**
@@ -94,6 +66,54 @@ public class CommissionComputation {
 		temp.add(String.format("%.2f", sales));
 		temp.add(String.format("%.2f", commission));
 		writeArray.add(temp);
+	}
+	
+	/**
+	 * Returns calculated commission for given employee within given period
+	 * @returns a double array, element 0 is total sales for the period, 1 is total commission
+	 * @throws ParseException 
+	 */
+	public static double[] getCommForPeriod(ArrayList<String> employee, Date start, Date end) throws ParseException {
+		ArrayList<Integer> validDates = getValidDateIndices(start, end);
+		double[] salesCom = new double[2];
+		String empPos = employee.get(4);
+		int empId = Integer.parseInt(employee.get(0));
+		double empSales = 0;
+		double empCom = 0;
+		for(int j=0; j<validDates.size(); j++) {
+			if((Integer.parseInt(salesRows.get(validDates.get(j)).get(0)) == empId ||
+					empPos.equalsIgnoreCase("President") || empPos.equalsIgnoreCase("Sales Manager"))) {
+				empSales += Double.parseDouble(salesRows.get(validDates.get(j)).get(3));
+			}
+		}
+		String tempCom = employee.get(7).replace('%', ' ');
+		if(!tempCom.isEmpty()) {
+			empCom = Double.parseDouble(tempCom)/100.0;
+		}
+		empCom = empCom * empSales;
+		
+		salesCom[0] = empSales;
+		salesCom[1] = empCom;		
+		
+		return salesCom;
+	}
+	
+	/**
+	 * Returns a list of indices of valid sales records within date period
+	 */
+	public static ArrayList<Integer> getValidDateIndices(Date startDate, Date endDate) throws ParseException {
+		ArrayList<Integer> fitCriteria = new ArrayList<Integer>(); //holds values of all included records		
+		Date readDate; //current dates of records as they are read in
+		
+		//Stores locations of included records in fitCriteria
+		for(int i=1; i<salesRows.size(); i++) {
+			readDate = format.parse(salesRows.get(i).get(1));
+			if((readDate.after(startDate) && readDate.before(endDate)) || readDate.equals(startDate) || readDate.equals(endDate)){
+				fitCriteria.add(i);
+			}
+		}
+		
+		return fitCriteria;
 	}
 		
 }
