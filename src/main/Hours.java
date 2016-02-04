03a67955c9c61aa65638c20746e3e59f2d8cb907
Jordan Long <jordan@promogroupllc.com>
2016-02-04 02:32:23 -0500

    Wrote methods to calculate pay, used Josh's function to compute commission and then calculated total in house. Only thing left is to convert dates so I can read from hours.txt

--- src/main/Hours.java
+++ src/main/Hours.java
@@ -5,6 +5,7 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
 import java.util.ArrayList;
 
 /**
@@ -19,24 +20,37 @@ public class Hours
 {
 
 	private static ArrayList<ArrayList<String>> employeeDB = FilesLab.getArrayListFromFile(FilesLabConstants.EMPLOYEE_INPUT_FILE); //Employee TXT File
-	private static ArrayList<ArrayList<String>> writeArray = new ArrayList<ArrayList<String>>(); //Array to be written to file
+	private static ArrayList<ArrayList<String>> payrollDB = new ArrayList<ArrayList<String>>(); //Array to be written to file
+	
+	private static Date startDate;
+	private static Date endDate;
+	
+	/**
+	 * Sub-ArrayLists of each department
+	 */
+	private static ArrayList<ArrayList<String>> AccountingList = new ArrayList<ArrayList<String>>();
+	private static ArrayList<ArrayList<String>> AdministrationList = new ArrayList<ArrayList<String>>();
+	private static ArrayList<ArrayList<String>> HRList = new ArrayList<ArrayList<String>>();
+	private static ArrayList<ArrayList<String>> OperationsList = new ArrayList<ArrayList<String>>();
+	private static ArrayList<ArrayList<String>> PurchasingList = new ArrayList<ArrayList<String>>();
+	private static ArrayList<ArrayList<String>> SalesList = new ArrayList<ArrayList<String>>();
+	
+	
+	
+	
 	
 	public static void main(String[] args) throws ParseException, IOException
 	{		
 		
-		
-		
-		ArrayList<Integer> fitCriteria = new ArrayList<Integer>(); //holds values of all included records		
-		
+			
 		
 		/**
 		 * Takes in command line arguments, aka date range and converts it to dates.
 		 */
 		
 		DateFormat format = new SimpleDateFormat("MM/dd/yy"); //converts date strings to dates
-		Date readDate; //current dates of records as they are read in
-		Date startDate = new Date(0);
-		Date endDate = new Date(Long.MAX_VALUE);
+		startDate = new Date(0);
+		endDate = new Date(Long.MAX_VALUE);
 		try {
 			startDate = format.parse(args[0]); //converts command line to date
 			endDate = format.parse(args[1]); //converts command line to date
@@ -45,23 +59,39 @@ public class Hours
 			System.out.println("Invalid date detected. Program will terminate.");
 			System.exit(0);
 		}
+		
+		
+		
+		/**
+		 * ------------------------------------------------------------------------------------------ *
+		 */
+		
+		
+		
+		findDepartment();											//Sort Employees
+		
+		
+		formatListHeader("Department: Accounting");					//Print header to file
+		checkWageType(AccountingList);								//Calculate Wage
+		formatListHeader("Department: Administration");
+		checkWageType(AdministrationList);
+		formatListHeader("Department: Human Resources");
+		checkWageType(HRList);
+		formatListHeader("Department: Operations");
+		checkWageType(OperationsList);
+		formatListHeader("Department: Purchasing");
+		checkWageType(PurchasingList);
+		formatListHeader("Department: Sales");
+		checkWageType(SalesList);
+
+		FilesLab.writeToFile(FilesLabConstants.HOURS_OUTPUT_FILE, payrollDB);	//Print final report to .txt
 	}
 	
 	
-		/**
-		 * Sub-ArrayLists of each department
-		 */
-		private static ArrayList<ArrayList<String>> AccountingList = new ArrayList<ArrayList<String>>();
-		private static ArrayList<ArrayList<String>> AdministrationList = new ArrayList<ArrayList<String>>();
-		private static ArrayList<ArrayList<String>> HRList = new ArrayList<ArrayList<String>>();
-		private static ArrayList<ArrayList<String>> OperationsList = new ArrayList<ArrayList<String>>();
-		private static ArrayList<ArrayList<String>> PurchasingList = new ArrayList<ArrayList<String>>();
-		private static ArrayList<ArrayList<String>> SalesList = new ArrayList<ArrayList<String>>();
 
-		
 	
 		// 1. Sort employees by department
-		public void findDepartment()
+		public static void findDepartment()
 		{
 			
 			for(int i=1; i<employeeDB.size(); i++)
@@ -98,60 +128,98 @@ public class Hours
 		
 		// 2. Check if hourly or salary-based pay level
 		
-		public void checkWageType(ArrayList<ArrayList<String>> list)
+		public static void checkWageType(ArrayList<ArrayList<String>> list) throws IOException, ParseException
 		{
+			double departmentPayrollTotal = 0.0;
+			
 			
 			for(int i=1; i<list.size(); i++)
 			{
+				
+				int employeeId = Integer.parseInt(list.get(i).get(0));
+				
+				//New row to store all employee financial information
+				ArrayList<String> newRow = new ArrayList<String>();
+				
+				
+				//Prints Employee's name to ArrayList
+				String name = "		"+list.get(i).get(1)+", "+list.get(i).get(2);
+				newRow.add(String.format("%d", name));
+				//newRow.set(i, name);
+			
+				double weeklyPay = 0.0;
+				
+				
 				if(list.get(i).get(6)=="Hour")
 				{
-					//Run hourly rate calc
+					//Run hourly rate calculator
 					//check if hours clocked fall within entered data parameters
+				
+					double hourlyRate = Double.parseDouble(list.get(i).get(5));
+					double hoursWorked = 0;
 					
-					int employeeId = Integer.parseInt(list.get(i).get(0));
-					double hourlyPay = 0;
 					
-					for(int j=0; j<employeeDB.size(); j++) 
-					{
-						if((Integer.parseInt(employeeDB.get(j).get(0))) == employeeId)			//Get employee pay rate from employeeDB
-							{
-								hourlyPay = Integer.parseInt(employeeDB.get(j).get(5));
-							}
-					}
+					
+					//Find hours within date range.
+					
+					DateFormat format = new SimpleDateFormat("MMM-dd"); //converts date strings to dates
+					
+					
+					
 					
 					//Check if hours are over 40, if so apply business rules.
-					if(hourlyPay>40)
+					if(hoursWorked>40)
 					{
-						double temp = hourlyPay-40.0;
-						temp = temp*1.5;
-						hourlyPay = hourlyPay+temp;
+						double temp = hoursWorked-40.0;
+						temp = temp*(hourlyRate*1.5);
+						weeklyPay = (40.0*hourlyRate)+temp;
 					}
-					
-					
-					//Get hours for this individual week and multiply them by employees hourly rate.
-					DateFormat format = new SimpleDateFormat("MMM-dd"); //converts date strings to dates
-					double hours;
-					double weeklyPay = hourlyPay*hours;
+					else
+					{
+						weeklyPay =  hoursWorked*hourlyRate;
+					}
 					
 					//Send final number to ArrayList.
-					
-					for(int i = 1, i<hours.size(), i++)
-					{
-						
-					}
+					newRow.add(String.format("%.2f", weeklyPay));
+
 				}
 				
 				else
 				{
-					//run commission calc
 					//Take salary and divide it by 52 for the weekly pay.
 					
-					double weeklyCommission = Integer.parseInt(employeeDB.get(i).get(6));
-					weeklyCommission = weeklyCommission/52;
+					double weeklySalary = Double.parseDouble(list.get(i).get(6));
+					weeklyPay = weeklySalary/52;
 					
 					//Send this to final array.
+					newRow.add(String.format("%.2f", weeklyPay));
 				}	
+				
+				
+				// 3. Check for commission using commission function in other java file.
+				
+				double[] commissionData = CommissionComputation.getCommForPeriod(list.get(i),startDate, endDate);
+				double weeklyCommission = commissionData[1];
+				
+				newRow.add(String.format("%.2f", weeklyCommission));
+				
+				
+				// 4. Add base pay and commission together to create total pay.
+				
+				double totalPay = weeklyPay+weeklyCommission;
+				departmentPayrollTotal+=totalPay;
+
+				//Add employee financial info row to payrollDB
+				payrollDB.add(newRow);
+				
 			}
+			
+			// 5. Add total department pay together to create Total Payroll.
+			
+			ArrayList<String> newRow = new ArrayList<String>();
+			newRow.add(String.format("%.2f", "Total Payroll:  " + departmentPayrollTotal));
+			payrollDB.add(newRow);
+
 			
 		}
 		
@@ -159,45 +227,12 @@ public class Hours
 		
 		
 		
-		
-		
-		
-		
-		
-		// 3. Check for commission from commission txt file.
-		
-		// 4. Add base pay and commission together to create total pay.
-		
-		// 5. Add total department pay together to create Total Payroll.
-	
-		
-
-		
-		
-		public static void calculateSalesForEmps(ArrayList<Integer> fitCriteria) {
-			for(int i=1; i<employeeDB.size(); i++) {
-				String employeePosition = employeeDB.get(i).get(4);
-				int empId = Integer.parseInt(employeeDB.get(i).get(0));
-				double employeeSales = 0;
-				double employeeCommission = 0;
-				for(int j=0; j<fitCriteria.size(); j++) {
-					if((Integer.parseInt(salesRows.get(fitCriteria.get(j)).get(0)) == empId ||
-							employeePosition.equalsIgnoreCase("President") || employeePosition.equalsIgnoreCase("Sales Manager"))) {
-						employeeSales += Double.parseDouble(salesRows.get(fitCriteria.get(j)).get(3));
-					}
-				}
-				String tempCom = employeeDB.get(i).get(7).replace('%', ' ');
-				if(!tempCom.isEmpty()) {
-					employeeCommission = Double.parseDouble(tempCom)/100.0;
-				}
-				employeeCommission = employeeCommission * employeeSales;
-				if(employeeSales != 0 && !alreadyInArray(empId))
-					addToWriteArray(empId, employeeSales, employeeCommission);
-			}
+		public static void formatListHeader(String input)
+		{
+			ArrayList<String> temp = new ArrayList<String>();
+			temp.add(input);
+			payrollDB.add(temp);
 		}
-	
-	
-	
-	
+		
 	
 }
