66e48ee456bd5a2aca3ffa3fd089e338e2c47b95
jl1361 <jl1361@mct162s07.cs.ship.edu>
2016-02-04 11:52:07 -0500

    Changed file lab constants for hours output to payroll.txt

--- src/main/FilesLabConstants.java
+++ src/main/FilesLabConstants.java
@@ -8,7 +8,7 @@ public class FilesLabConstants {
 	public static String EMPLOYEE_INPUT_FILE = "employees.txt";
 	public static String EMPLOYEE_OUTPUT_FILE = "employees.txt";
 	public static String HOURS_INPUT_FILE = "hours.txt";
-	public static String HOURS_OUTPUT_FILE = "testHours.txt";
+	public static String HOURS_OUTPUT_FILE = "payroll.txt";
 	public static String SALES_INPUT_FILE = "sales.txt";
 	public static String SALES_OUTPUT_FILE = "commissions.txt";
 }
